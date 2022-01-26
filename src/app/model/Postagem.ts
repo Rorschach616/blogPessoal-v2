731151d9dcf7580b2cf1f765694a8ba482333dc8
Matheus Gomes <mgrspeed@gmail.com>
2022-01-26 12:52:20 -0300

    @Rorschach616 [Task: ]

---
+++ src/app/model/Postagem.ts
@@ -0,0 +1,11 @@
+import { Tema } from "./Tema"
+import { User } from "./User"
+
+export class Postagem{
+public id: number
+public titulo: String
+public texto: String
+public data: Date
+public usuario: User
+public tema: Tema
+}
